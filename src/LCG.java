47858acf109edce626c2cbf064f726d2e7106bd6
HelgeJohannsen <hj@commerce-pro.de>
2022-04-28 13:30:20 +0200

    1 not finished

--- src/LCG.java
+++ src/LCG.java
@@ -1,11 +1,14 @@
+import java.text.CharacterIterator;
+import java.text.StringCharacterIterator;
+
 public class LCG {
         long a,b,m;
         long start;
 
         LCG(long startwert){
-            this.a = 15;
-            this.b = 759;
-            this.m = 146;
+            this.a = 1544897612;
+            this.b = 759423244;
+            this.m = 1464123068;
             setStartwert(startwert);
         }
         void setStartwert(long startwert){
@@ -23,5 +26,31 @@ public class LCG {
         lcg.nextRandom();
         lcg.nextRandom();
         lcg.nextRandom();
+    }
+
+    String lastByte(String inputByte){
+
+        String s = inputByte;
+        String mask = "00000000000000000000000011111111";
+        String result = "00000000000000000000000000000000";
+        CharacterIterator it = new StringCharacterIterator(s);
+        CharacterIterator itM = new StringCharacterIterator(mask);
+        System.out.println(s.length());
+        System.out.println(result.length());
+        while (it.current() != CharacterIterator.DONE)
+        {
+            char i = it.current();
+            char im = itM.current();
+            if(i == '1' && im == '1'){
+                System.out.print("1");
+                result.length();
+            }else{
+                System.out.print("0");
+            }
+            it.next();
+            itM.next();
+        }
+
+            return inputByte;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,29 @@
+import java.util.HashSet;
+
 public class Main {
 
     public static void main(String[] args) {
 	LCG lcg = new LCG(471);
+	long[] randoms = new long[256];
+	HashSet<Integer> zufallszahlen = new HashSet<>();
 	lcg.nextRandom();
     lcg.nextRandom();
+
+        for (int i = 0; i < 255; i++) {
+        //    randoms[i] = lcg.nextRandom();
+            zufallszahlen.add(Integer.parseInt( Long.toBinaryString(lcg.nextRandom() & 0xff),2));
+        }
+        for (int i = 0; i < 255; i++) {
+     //       System.out.print(Long.toBinaryString(randoms[i]) + " ");
+
+        }
+        System.out.println("length" + zufallszahlen.size());
+        long testLong = 542131418;
+        System.out.println("");
+      //  lcg.lastByte(Long.toBinaryString(testLong));
+        System.out.println(Long.toBinaryString(randoms[2]));
+        System.out.println(Long.toBinaryString(randoms[2] & 0xff));
+        System.out.println(Integer.parseInt( Long.toBinaryString(randoms[2] & 0xff),2));
+        System.out.println(Long.toHexString(randoms[2] & 0xff));
     }
 }
